51f5523627eba6eb4539592217ac2ea5d07ff24f
SubhamSalaria <shubhamsalaria786.ss@gmail.com>
2017-01-19 15:14:15 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Shubham
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Prints "Hello, World" 
+      System.out.println("Hello, World");
+      System.out.println("My github account name is SubhamSalaria");
+      System.out.println("My student Id is 200333595");
+      
+
+    }
+    
+}
